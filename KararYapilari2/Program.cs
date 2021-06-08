ac551d5b6edb2f717e8609cff6bd393c660bc954
arzutepe <38106558+arzutp@users.noreply.github.com>
2021-06-08 11:46:26 +0300

    Switch-Case

---
+++ KararYapilari2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace KararYapilari2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //hangi aydayız
+            int month = DateTime.Now.Month;
+
+            //Expression
+            switch (month)
+            {
+                case 1:
+                    Console.WriteLine("Ocak ayındayız");
+                    break;
+                case 2:
+                    Console.WriteLine("Şubat ayındayız");
+                    break;
+                case 3:
+                    Console.WriteLine("Mart ayındayız");
+                    break;
+                case 4:
+                    Console.WriteLine("Nisan ayındayız");
+                    break;
+                case 6:
+                    Console.WriteLine("Haziran ayındayız");
+                    break;
+                case 5:
+                    Console.WriteLine("Mayıs ayındayız");
+                    break;
+                
+                default:
+                    Console.WriteLine("Yanlış veri girdiniz");
+                    break;
+            }
+
+            switch (month)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    Console.WriteLine("Kış mevsimi");
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("İlkbahar mevsimi");
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Yaz mevsimi");
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Sonbahar mevsimi");
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
